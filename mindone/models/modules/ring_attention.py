06773ffc02dc0ac1e1dbbe5712db415ef71676d9
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-04-10 17:37:59 +0800

    Ring Attention v0.3: backbrop

--- mindone/models/modules/ring_attention.py
+++ mindone/models/modules/ring_attention.py
@@ -66,69 +66,59 @@ class RingAttention(nn.Cell):
         out = numerator / denominator
         return out, numerator, denominator, max_score
 
-    # def bprop(self, q, k, v, out, dout):
-    #     kv_comm = RingComm(process_group)
-    #     d_kv_comm = RingComm(process_group)
-    #     dq, dk, dv = None, None, None
-    #     next_dk, next_dv = None, None
-    #
-    #     block_dq_buffer = torch.empty(q.shape, dtype=q.dtype, device=q.device)
-    #     block_dk_buffer = torch.empty(k.shape, dtype=k.dtype, device=k.device)
-    #     block_dv_buffer = torch.empty(v.shape, dtype=v.dtype, device=v.device)
-    #
-    #     next_dk, next_dv = None, None
-    #     next_k, next_v = None, None
-    #
-    #     for step in range(kv_comm.world_size):
-    #         if step + 1 != kv_comm.world_size:
-    #             next_k = kv_comm.send_recv(k)
-    #             next_v = kv_comm.send_recv(v)
-    #             kv_comm.commit()
-    #         if step <= kv_comm.rank or not causal:
-    #             bwd_causal = causal and step == 0
-    #             dq, dk, dv, _, _, _, _ = grad(self.attn)(q, k, v, out, dout)
-    #
-    #             if dq is None:
-    #                 dq = block_dq_buffer.to(torch.float32)
-    #                 dk = block_dk_buffer.to(torch.float32)
-    #                 dv = block_dv_buffer.to(torch.float32)
-    #             else:
-    #                 dq += block_dq_buffer
-    #                 d_kv_comm.wait()
-    #                 dk = block_dk_buffer + next_dk
-    #                 dv = block_dv_buffer + next_dv
-    #         elif step != 0:
-    #             d_kv_comm.wait()
-    #             dk = next_dk
-    #             dv = next_dv
-    #
-    #         if step + 1 != kv_comm.world_size:
-    #             kv_comm.wait()
-    #             k = next_k
-    #             v = next_v
-    #
-    #         next_dk = d_kv_comm.send_recv(dk)
-    #         next_dv = d_kv_comm.send_recv(dv)
-    #         d_kv_comm.commit()
-    #
-    #     d_kv_comm.wait()
-    #
-    #     return dq, next_dk, next_dv
+    def bprop(self, q, k, v, out, dout):
+        out, num, denom, max_score = out
+        out_grad = dout[0]
+        dq, dk, dv = map(lambda x: ops.zeros_like(x), (q, k, v))
+        next_k, next_v = None, None
+
+        dl_part = (out_grad * out).sum(axis=-1, keepdims=True).swapaxes(1, 2)  # "bqhd,bqhd->bhq"
+
+        for step in range(self._num_devices):
+            if step < self._num_devices - 1:  # no need to exchange in the penultimate step
+                # BUG [MS2.2.10]: can't exchange both tensors at the same time
+                next_k = self._exchange((k,))[0]
+                next_v = self._exchange((v,))[0]
+
+            # 'bqhd,bkhd->bhqk'
+            attn_weights = ops.matmul(q.swapaxes(1, 2), k.permute((0, 2, 3, 1))) * self.scale
+
+            exp_weights = ops.exp(attn_weights - max_score) / denom.swapaxes(1, 2)
+
+            ds = ops.matmul(out_grad.swapaxes(1, 2), v.permute((0, 2, 3, 1)))  # "bqhd,bkhd->bhqk"
+            dl = (ds - dl_part) * exp_weights
+
+            # 'bhqk,bkhd->bqhd'
+            dq += ops.matmul(dl, k.permute(0, 2, 1, 3)).swapaxes(1, 2) * self.scale
+            # "bqhd,bhqk->bkhd"
+            dk += ops.matmul(q.permute(0, 2, 3, 1), dl).permute(0, 3, 1, 2) * self.scale
+            # "bhqk,bqhd->bkhd"
+            dv += ops.matmul(exp_weights.swapaxes(2, 3), out_grad.swapaxes(1, 2)).swapaxes(1, 2)
+
+            if step < self._num_devices - 1:
+                # TODO: ensure that next_k and next_v are received (necessary?)
+                k = self._depend(next_k, next_k)
+                v = self._depend(next_v, next_v)
+
+            dk = self._exchange((dk,))[0]
+            dv = self._exchange((dv,))[0]
+
+        return dq, dk, dv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     import sys
-    import mindspore as ms
+
     import numpy as np
 
+    import mindspore as ms
     from mindspore.communication import get_group_size, get_rank, init
 
     sys.path.append("../../../")
     from mindone.models.dit import Attention
 
     ms.set_seed(42)
-
 
     class DistribExec(nn.Cell):
         def __init__(self, model, rank_id, num_devices):
@@ -147,9 +137,9 @@ if __name__ == '__main__':
             q_size = q.shape[1] // self._num_devices
             kv_size = k.shape[1] // self._num_devices
 
-            q = q[:, self._rank_id * q_size:(self._rank_id + 1) * q_size]
-            k = k[:, self._rank_id * kv_size:(self._rank_id + 1) * kv_size]
-            v = v[:, self._rank_id * kv_size:(self._rank_id + 1) * kv_size]
+            q = q[:, self._rank_id * q_size : (self._rank_id + 1) * q_size]
+            k = k[:, self._rank_id * kv_size : (self._rank_id + 1) * kv_size]
+            v = v[:, self._rank_id * kv_size : (self._rank_id + 1) * kv_size]
 
             out, numerator, denominator, max_score = self._model(q, k, v)
 
@@ -160,14 +150,17 @@ if __name__ == '__main__':
 
             return out, numerator, denominator, max_score
 
-
     device_id = int(os.getenv("DEVICE_ID"))
-    ms.set_context(mode=ms.GRAPH_MODE, device_id=device_id, deterministic="ON",
-                   ascend_config={"precision_mode": "must_keep_origin_dtype"})
+    ms.set_context(
+        mode=ms.GRAPH_MODE,
+        device_id=device_id,
+        deterministic="ON",
+        ascend_config={"precision_mode": "must_keep_origin_dtype"},
+    )
     init()
     device_num = get_group_size()
     rank_id = get_rank()
-    print(f"Device_id: {device_id}, rank_id: {rank_id}, device_num: {device_num}")
+
     ms.set_auto_parallel_context(
         parallel_mode=ms.ParallelMode.HYBRID_PARALLEL,
         # gradients_mean=True,
@@ -182,35 +175,64 @@ if __name__ == '__main__':
 
     ra = RingAttention(dim_head=q_.shape[-1], rank_id=rank_id, num_devices=device_num)
     ra_dist = DistribExec(ra, rank_id, device_num)
-    ra_dist.set_train(False)
 
-    out_ = ra_dist(q_, k_, v_)
-    for item in out_:
-        print("-" * 100, "\n", item.shape)
+    print("Forward pass...")
+    ra_dist.set_train(False)
+    out1_fwd = ra_dist(q_, k_, v_)[0].asnumpy()
+
+    print("Backward pass...")
+    ra_dist.set_train(True)
+
+    labels = ms.Tensor(np.ones((10, 256, 16, 128)), dtype=ms.float32)
+    loss = nn.MSELoss()
+
+    def forward_ra(q, k, v, label):
+        z = ra_dist(q, k, v)
+        loss_ = loss(z[0], label)
+        return loss_
+
+    grad1 = grad(forward_ra)(q_, k_, v_, labels).asnumpy()
 
     if rank_id == 0:
+        vanilla_attn = Attention(q_.shape[-1])
+
         def _rearange_in(x: ms.Tensor):
             # (b, n, h, d) -> (b*h, n, d)
             x = x.swapaxes(1, 2).reshape(-1, x.shape[1], x.shape[3])
             return x
-
 
         def _rearange_out(x: ms.Tensor, h: int):
             # (b*h, n, d) -> (b, n, h, d)
             x = x.reshape(-1, h, x.shape[1], x.shape[2]).swapaxes(1, 2)
             return x
 
-
         def test_forward():
-            attn2 = Attention(q_.shape[-1])
-            attn2.set_train(False)
+            vanilla_attn.set_train(False)
 
-            out1 = out_[0].asnumpy()
-            out2 = attn2(_rearange_in(q_), _rearange_in(k_), _rearange_in(v_), None)
+            out2 = vanilla_attn(_rearange_in(q_), _rearange_in(k_), _rearange_in(v_), None)
             out2 = _rearange_out(out2, q_.shape[2]).asnumpy()
 
             print("-" * 100)
-            print(np.allclose(out1, out2, atol=1e-5))
-            print(abs(out1 - out2).max())
+            print(np.allclose(out1_fwd, out2, atol=1e-5))
+            print(abs(out1_fwd - out2).max())
 
+        def test_bprop():
+            vanilla_attn.set_train(True)
+
+            def forward_vanilla(q, k, v, label):
+                z = vanilla_attn(q, k, v, None)
+                z = _rearange_out(z, q_.shape[2])
+                loss_ = loss(z, label)
+                return loss_
+
+            grad2 = grad(forward_vanilla)(_rearange_in(q_), _rearange_in(k_), _rearange_in(v_), labels)
+            grad2 = _rearange_out(grad2, q_.shape[2]).asnumpy()
+
+            print("-" * 100)
+            print(np.allclose(grad1, grad2, atol=1e-5))
+            print(abs(grad1 - grad2).max())
+
+        print("Testing forward pass...")
         test_forward()
+        print("Testing backward pass...")
+        test_bprop()
