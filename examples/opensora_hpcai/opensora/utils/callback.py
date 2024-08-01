b3537daaf6597105c11e0ddc16ce41a0ed810f96
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-08-01 10:39:38 +0800

    add validation result saving in callback

--- examples/opensora_hpcai/opensora/utils/callback.py
+++ examples/opensora_hpcai/opensora/utils/callback.py
@@ -3,14 +3,17 @@ import os
 import time
 from typing import List
 
-from mindspore.train import Callback
+import numpy as np
+
+from mindspore.train import Callback, RunContext
 
 _logger = logging.getLogger(__name__)
 
 
 class PerfRecorder(Callback):
     """
-    Improved version of `mindone.trainers.recorder.PerfRecorder` used here for testing first.
+    Improved version of `mindone.trainers.recorder.PerfRecorder` that tracks validation metrics as well.
+    Used here first for testing.
     """
 
     def __init__(
@@ -34,28 +37,29 @@ class PerfRecorder(Callback):
             with open(self._log_file, "w", encoding="utf-8") as fp:
                 fp.write(header + "\n")
 
-    def on_train_step_begin(self, run_context):
+    def on_train_step_begin(self, run_context: RunContext):
         self._step_time = time.perf_counter()
 
-    def on_train_step_end(self, run_context):
-        step_time = self._step_time - time.perf_counter()
+    def on_train_step_end(self, run_context: RunContext):
+        step_time = time.perf_counter() - self._step_time
         cb_params = run_context.original_args()
         cur_step = cb_params.cur_step_num
         loss = cb_params.net_outputs
+        loss = loss[0].asnumpy() if isinstance(loss, tuple) else np.mean(loss.asnumpy())
 
         with open(self._log_file, "a", encoding="utf-8") as fp:
-            fp.write(f"{cur_step}{self._sep}{loss.item()}{self._sep}{step_time}\n")
+            fp.write(f"{cur_step:<8}{self._sep}{loss.item():<10.6f}{self._sep}{step_time:<8.3f}\n")
 
-    def on_eval_end(self, run_context):
+    def on_eval_end(self, run_context: RunContext):
         cb_params = run_context.original_args()
         cur_step = "-"
         step_time = "-"
-        loss = cb_params.net_outputs
+        loss = "-"
 
-        metrics = cb_params.get("metrics")
+        metrics = cb_params.metrics
         try:
             metrics = self._sep.join([f"{metrics[m]:.4f}" for m in self._metrics])
         except KeyError:
             raise KeyError(f"Metric ({self._metrics}) not found in eval result ({list(metrics.keys())}).")
         with open(self._log_file, "a", encoding="utf-8") as fp:
-            fp.write(f"{cur_step}{self._sep}{loss.item()}{self._sep}{step_time}{self._sep}{metrics}\n")
+            fp.write(f"{cur_step:<8}{self._sep}{loss:<10}{self._sep}{step_time:<8}{self._sep}{metrics}\n")
