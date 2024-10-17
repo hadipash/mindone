ccb584b6941b7088b5b0bb7312de149ff1798309
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2024-10-17 11:50:57 +0800

    add ByT5

--- examples/movie_gen/tests/ut/test_byt5_pynative.py
+++ examples/movie_gen/tests/ut/test_byt5_pynative.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import numpy as np
 import torch
 from transformers import AutoTokenizer
@@ -5,11 +8,13 @@ from transformers import T5EncoderModel as T5EncoderModel_PyTorch
 
 import mindspore as ms
 
+# FIXME: remove in future when mindone is ready for install
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../.."))
 from mindone.transformers import T5EncoderModel
 
 ms.set_context(mode=ms.PYNATIVE_MODE)
 
-fp32_fwd_tolerance = 1e-5
+fp32_tolerance = 1e-4
 
 test_samples = [
     "Life is like a box of chocolates.",
@@ -18,18 +23,18 @@ test_samples = [
     "Aujourd'hui c'est lundi.",
 ]
 
-tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
+tokenizer = AutoTokenizer.from_pretrained("google/byt5-small", local_files_only=True)
 test_samples = tokenizer(test_samples, padding="longest", return_tensors="np")
 
 byt5_ms = T5EncoderModel.from_pretrained("google/byt5-small", local_files_only=True)
 byt5_pt = T5EncoderModel_PyTorch.from_pretrained("google/byt5-small", local_files_only=True)
 
 
-def test_forward_fp32():
+def test_fp32():
     ms_enc = byt5_ms(
         ms.Tensor(test_samples.input_ids, dtype=ms.int32), ms.Tensor(test_samples.attention_mask, dtype=ms.uint8)
     )
     ms_enc = ms_enc[0].asnumpy().astype(np.float32)
     pt_enc = byt5_pt(torch.tensor(test_samples.input_ids), torch.tensor(test_samples.attention_mask), return_dict=False)
     pt_enc = pt_enc[0].detach().numpy().astype(np.float32)
-    assert np.allclose(ms_enc, pt_enc, atol=fp32_fwd_tolerance, rtol=0)
+    assert np.allclose(ms_enc, pt_enc, atol=fp32_tolerance, rtol=0)
